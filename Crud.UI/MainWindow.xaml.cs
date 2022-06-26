0dbe9d6c9c490737d04f4c9500f0a88fd5d8a1a9
azelf longo <az.elf.longo@gmail.com>
2022-06-27 02:53:59 +0300

    del comment

--- Crud.UI/MainWindow.xaml.cs
+++ Crud.UI/MainWindow.xaml.cs
@@ -72,50 +72,8 @@ namespace Crud.UI
                 
            
         }
-        #region save as
-        /* private Task CreateEmployee(string txtEmployeeName, string txtEmployeeSurname)
-         {
-              Awa
-
-              _services.AddEmployee(txtEmployeeName, txtEmployeeSurname);
-
-
-
-         }*/
-
-        /* private async Task InitializeIfNeededAsync() => await Task.Delay(1);
-         public struct DetachSynchronizationContextAwaiter : ICriticalNotifyCompletion
-         {
-             /// <summary>
-             /// Returns true if a current synchronization context is null.
-             /// It means that the continuation is called only when a current context
-             /// is presented.
-             /// </summary>
-             public bool IsCompleted => SynchronizationContext.Current == null;
-
-             public void OnCompleted(Action continuation)
-             {
-                 ThreadPool.QueueUserWorkItem(state => continuation());
-             }
-
-             public void UnsafeOnCompleted(Action continuation)
-             {
-                 ThreadPool.UnsafeQueueUserWorkItem(state => continuation(), null);
-             }
-
-             public void GetResult() { }
-
-             public DetachSynchronizationContextAwaiter GetAwaiter() => this;
-         }
-
-         public static class Awaiters
-         {
-             public static DetachSynchronizationContextAwaiter DetachCurrentSyncContext()
-             {
-                 return new DetachSynchronizationContextAwaiter();
-             }
-         }*/
-        #endregion
+        
+     
         private async Task ListEmployees ()
         {
             var employeesList = await _services.ListEmployees();
